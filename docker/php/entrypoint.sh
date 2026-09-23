ce8821679bef032e4769b6d141812f95a4af80b5
Roberto Crofford T. <rcroffordt@duoc.cl>
2026-09-23 16:08:18 -0300

    fix: crear cache al iniciar contenedores

--- docker/php/entrypoint.sh
+++ docker/php/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -eu
 
-mkdir -p storage/framework/cache storage/framework/sessions storage/framework/views storage/logs
+mkdir -p bootstrap/cache storage/framework/cache storage/framework/sessions storage/framework/views storage/logs
 chown -R www-data:www-data storage bootstrap/cache
 
 exec "$@"
